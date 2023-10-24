28ef528b0aad74b11d0abdb286ae7fab829d433a
duncan91-ops <duncankithinji91@gmail.com>
2023-10-24 11:48:13 +0300

    complete hero section of the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import type { Metadata } from "next";
 import styles from "./page.module.scss";
 
@@ -7,8 +8,24 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className={`${styles.main} container`}>
-      <h1>Homepage</h1>
+    <main className={styles.main}>
+      <section className={styles.hero}>
+        <div className={styles.hero__content}>
+          <h1 className={styles.title}>
+            Award-winning custom designs and digital branding solutions
+          </h1>
+          <p className={styles.msg}>
+            With over 10 years in the industry, we are experienced in creating
+            fully responsive websites, app design, and engaging brand
+            experiences. Find out more about our services.
+          </p>
+          <Link href="/" className="btn__primary">
+            learn more
+          </Link>
+        </div>
+      </section>
+      <nav className={styles.nav}></nav>
+      <section className={styles.values}></section>
     </main>
   );
 }
